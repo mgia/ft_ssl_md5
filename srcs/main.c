a1a132e99e380fea64286d94f888f20db33c50e2
Marc G Tan <findmarcg@gmail.com>
2018-08-26 17:43:24 -0400

    Added SHA256 function

--- srcs/main.c
+++ srcs/main.c
@@ -28,6 +28,7 @@ void	parse_flags(t_info *d, char **av, int ac)
 	d->offset = i;
 }
 
+
 void	initialize(t_info *d, char **av, int ac)
 {
 	d->f_p = 0;
@@ -92,50 +93,19 @@ char		*pad_zeros(char *str)
 
 #include "libft.h"
 
-static int	ft_get_len(uint32_t n, int base)
-{
-	int i;
-
-	i = 0;
-	while (n != 0)
-	{
-		n = n / base;
-		i++;
-	}
-	return (i);
-}
-
-static char	*free_str(char *str)
-{
-	char *tmp;
-
-	tmp = str;
-	str = ft_strsub(str, 1, ft_strlen(str) - 1);
-	free(tmp);
-	return (str);
-}
 
 char		*unsigned_itoa_base(uint32_t n, int base)
 {
-	int		len;
 	char	*str;
-	char	*cmp;
+	char	*tmp;
 
-	cmp = "0123456789abcdef";
-	len = ft_get_len(n, base);
-	if (n == 0)
-		return (ft_strdup("0"));
-	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	str[len + 1] = 0;
-	while (len + 1)
-	{
-		str[len] = cmp[n % base];
-		n = n / base;
-		len--;
-	}
+	str = ft_itoa_base(n, base);
 	if (str[0] == '0')
-		str = free_str(str);
+	{
+		tmp = str;
+		str = ft_strsub(str, 1, ft_strlen(str) - 1);
+		free(tmp);
+	}
 	return (str);
 }
 
@@ -163,21 +133,6 @@ uint32_t	*init_md5_buffer(void)
 	buffer[2] = 0x98badcfe;
 	buffer[3] = 0x10325476;
 	return (buffer);
-}
-
-void	*hi_bzero(void *s, size_t n)
-{
-	unsigned char	*i;
-	size_t			b;
-
-	b = 0;
-	i = (unsigned char *)s;
-	while (b < n)
-	{
-		i[b] = '\0';
-		b++;
-	}
-	return (s);
 }
 
 unsigned char	*pad_md5_msg(unsigned char *init_msg, size_t *len)
@@ -250,16 +205,15 @@ void	md5(unsigned char *init_msg, size_t len)
 {
 	uint32_t	*buffer;
 	unsigned char	*msg;
-	size_t		offset;
-	int			i;
+	size_t		i;
 
 	buffer = init_md5_buffer();
 	msg = pad_md5_msg(init_msg, &len);
-	offset = 0;
-	while (offset < len)
+	i = 0;
+	while (i < len)
 	{
-		process_chunk(msg + offset, buffer);
-		offset += 64;
+		process_chunk(msg + i, buffer);
+		i += 64;
 	}
 	i = -1;
 	while (++i < 4)
@@ -269,9 +223,187 @@ void	md5(unsigned char *init_msg, size_t len)
 	free(msg);
 }
 
-void	exec_sha256(char *init_msg)
+const uint32_t sha256_k[] = {
+	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+	0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,
+	0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa,
+	0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+	0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138,
+	0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624,
+	0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+	0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f,
+	0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
+struct	s_sha256_buffer
 {
-	ft_printf("hash/sha265/%s", init_msg);
+	uint32_t	a;
+	uint32_t	b;
+	uint32_t	c;
+	uint32_t	d;
+	uint32_t	e;
+	uint32_t	f;
+	uint32_t	g;
+	uint32_t	h;
+};
+
+typedef struct s_sha256_buffer	t_sha256;
+
+uint32_t	*init_sha256_buffer(void)
+{
+	uint32_t	*buffer;
+
+	buffer = (uint32_t *)ft_memalloc(8 * sizeof(uint32_t));
+	buffer[0] = 0x6a09e667;
+	buffer[1] = 0xbb67ae85;
+	buffer[2] = 0x3c6ef372;
+	buffer[3] = 0xa54ff53a;
+	buffer[4] = 0x510e527f;
+	buffer[5] = 0x9b05688c;
+	buffer[6] = 0x1f83d9ab;
+	buffer[7] = 0x5be0cd19;
+	return (buffer);
+}
+
+void	add_big_endian_integer(unsigned char *msg, size_t len, size_t new_len)
+{
+	msg[new_len / 8 - 8] = (char)(len * 8 >> (8 * 7));
+	msg[new_len / 8 - 7] = (char)(len * 8 >> (8 * 6));
+	msg[new_len / 8 - 6] = (char)(len * 8 >> (8 * 5));
+	msg[new_len / 8 - 5] = (char)(len * 8 >> (8 * 4));
+	msg[new_len / 8 - 4] = (char)(len * 8 >> (8 * 3));
+	msg[new_len / 8 - 3] = (char)(len * 8 >> (8 * 2));
+	msg[new_len / 8 - 2] = (char)(len * 8 >> (8 * 1));
+	msg[new_len / 8 - 1] = (char)(len * 8 >> (8 * 0));
+}
+
+unsigned char	*pad_sha256_msg(unsigned char *init_msg, size_t *len)
+{
+	unsigned char	*msg;
+	size_t			bit_size;
+	size_t			new_len;
+
+	bit_size = *len * 8;
+	new_len = ((bit_size + 64) / 512) * 512 + 448 + 64;
+	msg = (unsigned char *)ft_memalloc(new_len / 8 + 8);
+	ft_memcpy(msg, init_msg, *len);
+	msg[*len] = 128;
+	add_big_endian_integer(msg, *len, new_len);
+	*len = new_len / 8;
+	return (msg);
+}
+
+uint32_t	right_rotate(uint32_t x, int c)
+{
+	return (x >> c) | (x << (32 - c));
+}
+
+void init_msg_schedule_arr(uint32_t *words, uint32_t m_arr[64])
+{
+	uint32_t	s0;
+	uint32_t	s1;
+	int			i;
+
+	i = -1;
+	while (++i < 16)
+		m_arr[i] = __builtin_bswap32(words[i]);
+	while (i < 64)
+	{
+		s0 = right_rotate(m_arr[i - 15], 7) ^ right_rotate(m_arr[i - 15], 18) ^
+				(m_arr[i - 15] >> 3);
+		s1 = right_rotate(m_arr[i - 2], 17) ^ right_rotate(m_arr[i - 2], 19) ^
+				(m_arr[i - 2] >> 10);
+		m_arr[i] = (m_arr[i - 16] + s0 + m_arr[i - 7] + s1);
+		i++;
+	}
+}
+
+void	init_working_variables(t_sha256 *buf, uint32_t *buffer)
+{
+	buf->a = buffer[0];
+	buf->b = buffer[1];
+	buf->c = buffer[2];
+	buf->d = buffer[3];
+	buf->e = buffer[4];
+	buf->f = buffer[5];
+	buf->g = buffer[6];
+	buf->h = buffer[7];
+}
+
+void	append_to_hash(t_sha256 *buf, uint32_t *buffer)
+{
+	buffer[0] += buf->a;
+	buffer[1] += buf->b;
+	buffer[2] += buf->c;
+	buffer[3] += buf->d;
+	buffer[4] += buf->e;
+	buffer[5] += buf->f;
+	buffer[6] += buf->g;
+	buffer[7] += buf->h;
+}
+
+void	compression_function(t_sha256 *buf, uint32_t m_arr[64], int i)
+{
+	uint32_t s0;
+	uint32_t s1;
+	uint32_t maj;
+	uint32_t ch;
+	uint32_t temp1;
+
+	while (i < 64)
+	{
+		s1 = right_rotate(buf->e, 6) ^ right_rotate(buf->e, 11) ^
+				right_rotate(buf->e, 25);
+		ch = (buf->e & buf->f) ^ ((~(buf->e)) & buf->g);
+		temp1 = buf->h + s1 + ch + sha256_k[i] + m_arr[i];
+		s0 = right_rotate(buf->a, 2) ^ right_rotate(buf->a, 13) ^
+				right_rotate(buf->a, 22);
+		maj = (buf->a & buf->b) ^ (buf->a & buf->c) ^ (buf->b & buf->c);
+
+		buf->h = buf->g;
+		buf->g = buf->f;
+		buf->f = buf->e;
+		buf->e = buf->d + temp1;
+		buf->d = buf->c;
+		buf->c = buf->b;
+		buf->b = buf->a;
+		buf->a = temp1 + s0 + maj;
+		i++;
+	}
+}
+
+void	process_sha256_chunk(unsigned char *chunk, uint32_t *buffer)
+{
+	uint32_t	*words;
+	uint32_t	m_arr[64];
+	t_sha256	buf;
+
+	words = (uint32_t *)chunk;
+	init_msg_schedule_arr(words, m_arr);
+	init_working_variables(&buf, buffer);
+	compression_function(&buf, m_arr, 0);
+	append_to_hash(&buf, buffer);
+}
+
+void	sha256(unsigned char *init_msg, size_t len)
+{
+	uint32_t		*buffer;
+	unsigned char	*msg;
+	size_t			i;
+
+	buffer = init_sha256_buffer();
+	msg = pad_sha256_msg(init_msg, &len);
+	i = 0;
+	while (i < len)
+	{
+		process_sha256_chunk(msg + i, buffer);
+		i += 64;
+	}
+	print_hash(buffer, 8);
+	free(msg);
+	free(buffer);
 }
 
 void	run_hash(char *str, char *hash)
@@ -279,7 +411,7 @@ void	run_hash(char *str, char *hash)
 	if (!ft_strcmp(hash, "md5"))
 		md5((uint8_t *)str, ft_strlen(str));
 	else if (!ft_strcmp(hash, "sha256"))
-		exec_sha256(str);
+		sha256((unsigned char *)str, ft_strlen(str));
 }
 
 #include <stdio.h>
@@ -315,24 +447,40 @@ int		get_file(t_info *d, char *name, char *hash)
 
 	if ((fd = open(name, O_RDWR)) < 0)
 	{
-		ft_printf("ft_ssl: %s: %s: No such directory found\n", name, hash);
+		ft_printf("ft_ssl: %s: %s: No such file or directory found\n", name, hash);
 		return (0);
 	}
 	d->arg = get_str(fd);
 	return (1);
 }
 
+char	*ft_str_toupper(char *str)
+{
+	char	*res;
+	int		i;
+
+	res = ft_strdup(str);
+	i = -1;
+	while (res[++i])
+		res[i] = ft_toupper(res[i]);
+	return (res);
+}
+
 void	print_entry(t_info *d, char *str, char *hash, int file)
 {
+	char *tmp;
+
+	tmp = ft_str_toupper(hash);
 	if (!d->f_q)
 	{
 		if (file)
-			ft_printf("%s (%s) = ", hash, str);
+			ft_printf("%s (%s) = ", tmp, str);
 		else
-			ft_printf("%s (\"%s\") = ", hash, str);
+			ft_printf("%s (\"%s\") = ", tmp, str);
 	}
 	run_hash(file ? d->arg : str, hash);
 	ft_putchar('\n');
+	free(tmp);
 }
 
 void	print_entry_rev(t_info *d, char *str, char *hash, int file)
